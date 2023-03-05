1161b17d7589beaf33fea68389f7e2d8024327e2
cario <cario@wix.com>
2023-03-05 16:00:33 +0200

    refactor: assets ref

--- src/components/goals/goals.tsx
+++ src/components/goals/goals.tsx
@@ -1,9 +1,13 @@
 import { IconButton } from '../icon-button/icon-button';
 import { GoalItem } from './goal-item/goal-item';
 import styles from './goals.module.scss';
-import holidaysImageUrl from '../../assets/images/illustrations/mountain.svg';
-import renovationImageUrl from '../../assets/images/illustrations/brush.svg';
-import xboxImageUrl from '../../assets/images/illustrations/controller.svg';
+
+const holidaysImageUrl =
+    'https://static.wixstatic.com/shapes/610b66_71524cee9a8c43de91e86b0ecc9f9cab.svg'; // mountain.svg (36x36)
+const renovationImageUrl =
+    'https://static.wixstatic.com/shapes/610b66_0d486933d56942b48bfed545eba3f3e2.svg'; // brush.svg (36x36)
+const xboxImageUrl =
+    'https://static.wixstatic.com/shapes/610b66_ca99e04d140f4b8a9633dd902d2cb1b8.svg'; // controller.svg (37x36)
 
 export const Goals = () => {
     const goals = [
